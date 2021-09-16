ed736d8bbb79ddd51f2138307513d72af7738395
Mauricio Soares da Silva <mau.mss@gmail.com>
2021-09-16 16:53:04 -0300

    Separa lógica de negócio
    Cria classe value object e de serviço para calcular o rateio.

---
+++ RateioService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rateio
+{
+    class RateioService
+    {
+        public void RatearCustos(RateioVO rateioVO)
+        {
+            MoedaConverter moedaConverter = new MoedaConverter();
+            double custoRestante = rateioVO.CustoTotal;
+            for (int i = 0; i < rateioVO.Items.Count; i++)
+            {
+                string ticket = rateioVO.Items[i].SubItems[0].Text;
+                double valor = moedaConverter.ConverterDeMoeda(rateioVO.Items[i].SubItems[1].Text);
+                if (valor == 0.0) return;
+                double rateio = 0.0;
+                // calcula rateio
+                if ((i + 1) == rateioVO.Items.Count)
+                {
+                    rateio = custoRestante;
+                }
+                else
+                {
+                    rateio = (valor / (rateioVO.TotalCompras + rateioVO.TotalVendas)) * rateioVO.CustoExcetoCorretagem + rateioVO.Corretagem;
+                    rateio = Math.Round(rateio, 2);
+                    custoRestante -= rateio;
+                }
+                rateioVO.Items[i].SubItems[2].Text = moedaConverter.ConverterParaMoeda(rateio.ToString());
+            }
+        }
+    }
+}
